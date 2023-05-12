f2858cce1e47824f6b86d88b5e957c54d9fe8a2e
Kurt Carvey Cadenas <kurtcarveycadenas2001@gmail.com>
2023-05-12 12:39:31 +0800

    Update dashboard.blade.php

--- resources/views/patient/dashboard.blade.php
+++ resources/views/patient/dashboard.blade.php
@@ -81,47 +81,47 @@
                     <div class="table-responsive">
                         <table class="table table-borderless" id="dataTables" width="100%" cellspacing="0">
                             <thead class="bg-primary text-light">
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Time</th>
-                                    <th>Day</th>
-                                    <th>Patient</th>
-                                    <th>Doctor</th>
-                                    <th>Status</th>
-                                    <th>Actions</th>
-                                </tr>
+                            <tr>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>Day</th>
+                                <th>Patient</th>
+                                <th>Doctor</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
                             </thead>
 
                             <tbody>
-                                @foreach ($appointments as $appointment)
-                                    <tr>
-                                        <td>{{ $appointment->schedule->formatted_date }}</td>
-                                        <td>{{ $appointment->formatted_time }}</td>
-                                        <td>{{ $appointment->schedule->day }}</td>
-                                        <td>{{ $appointment->patient->full_name }}</td>
-                                        <td>{{ $appointment->doctor->full_name }}</td>
-                                        <td>
-                                            @if ($appointment->status == 'pending')
-                                                <span class="badge rounded-pill text-bg-secondary">
+                            @foreach ($appointments as $appointment)
+                                <tr>
+                                    <td>{{ $appointment->schedule->formatted_date }}</td>
+                                    <td>{{ $appointment->formatted_time }}</td>
+                                    <td>{{ $appointment->schedule->day }}</td>
+                                    <td>{{ $appointment->patient->full_name }}</td>
+                                    <td>{{ $appointment->doctor->full_name }}</td>
+                                    <td>
+                                        @if ($appointment->status == 'pending')
+                                            <span class="badge rounded-pill text-bg-secondary">
                                                     {{ $appointment->formatted_status }}</span>
-                                            @elseif($appointment->status == 'confirmed')
-                                                <span class="badge rounded-pill text-bg-primary">
+                                        @elseif($appointment->status == 'confirmed')
+                                            <span class="badge rounded-pill text-bg-primary">
                                                     {{ $appointment->formatted_status }}</span>
-                                            @elseif($appointment->status == 'completed')
-                                                <span class="badge rounded-pill text-bg-success">
+                                        @elseif($appointment->status == 'completed')
+                                            <span class="badge rounded-pill text-bg-success">
                                                     {{ $appointment->formatted_status }}</span>
-                                            @elseif($appointment->status == 'cancelled')
-                                                <span class="badge rounded-pill text-bg-danger">
+                                        @elseif($appointment->status == 'cancelled')
+                                            <span class="badge rounded-pill text-bg-danger">
                                                     {{ $appointment->formatted_status }}</span>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <a class="btn btn-primary btn-sm"
-                                                href="{{ route('patient.appointments.show', $appointment->id) }}"
-                                                role="button"><i class="bi bi-info-circle-fill"></i></a>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-primary btn-sm"
+                                           href="{{ route('patient.appointments.show', $appointment->id) }}"
+                                           role="button"><i class="bi bi-info-circle-fill"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -129,7 +129,6 @@
             </div>
         </div>
     </div>
-
 @endsection
 
 @push('scripts')
