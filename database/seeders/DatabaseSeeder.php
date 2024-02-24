30520618979451889996c4a7822a6c0f589fa279
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-02-24 10:00:03 +0800

    Add roles and create users in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,8 +20,79 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\User::factory(1)->create();
-        // \App\Models\Doctor::factory(1)->create();
-        \App\Models\Admin::factory(1)->create();
+        $admin = \Spatie\Permission\Models\Role::create(
+            [
+                "name" => "admin",
+            ]
+        );
+
+        $doctor = \Spatie\Permission\Models\Role::create(
+            [
+                "name" => "doctor",
+            ]
+        );
+
+        $patient = \Spatie\Permission\Models\Role::create(
+            [
+                "name" => "patient",
+            ]
+        );
+
+        \App\Models\User::factory()->create([
+            'username' => 'admin',
+            'first_name' => 'Juan',
+            'last_name' => 'Dela Cruz',
+            'email' => 'admin@example.com',
+            'contact_number' => '09123456789',
+            'password' => Hash::make('admin1234'),
+        ])->assignRole($admin);
+
+        \App\Models\User::factory()->create([
+            'username' => 'doctor',
+            'first_name' => 'Joseph',
+            'last_name' => 'Dela Cruz',
+            'email' => 'doctor@example.com',
+            'contact_number' => '09123456789',
+            'password' => Hash::make('doctor1234'),
+        ])->assignRole($doctor);
+
+        \App\Models\User::factory()->create([
+            'username' => 'patient',
+            'first_name' => 'Mark',
+            'last_name' => 'Dela Cruz',
+            'email' => 'patient@example.com',
+            'contact_number' => '09123456789',
+            'password' => Hash::make('patient1234'),
+        ])->assignRole($patient);
+
+        \App\Models\Admin::factory()->create([
+            'user_id' => 1,
+            'first_name' => 'Juan',
+            'last_name' => 'Dela Cruz',
+            'email' => 'admin@example.com',
+            'birthday' => '2001-11-14',
+            'contact_number' => '09123456789',
+            'address' => 'Caloocan City',
+        ]);
+
+        \App\Models\Doctor::factory()->create([
+            'user_id' => 2,
+            'first_name' => 'Joseph',
+            'last_name' => 'Dela Cruz',
+            'email' => 'doctor@example.com',
+            'birthday' => '2001-11-14',
+            'contact_number' => '09123456789',
+            'address' => 'Caloocan City',
+        ]);
+
+        \App\Models\Patient::factory()->create([
+            'user_id' => 3,
+            'first_name' => 'Mark',
+            'last_name' => 'Dela Cruz',
+            'email' => 'patient@example.com',
+            'birthday' => '2001-11-14',
+            'contact_number' => '09123456789',
+            'address' => 'Caloocan City',
+        ]);
     }
 }
